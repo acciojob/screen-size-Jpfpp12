5d9536774029b2fe445abd439583ac8ee43f6e1d
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-03-25 22:41:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 //your JS code here. If required.
-function updateWindowSize() {
-const width = window.innerWidth;
-const height = window.innerHeight;
-document.getElementById('width').textContent = width;
-document.getElementById('height').textContent = height;
- }
-window.addEventListener('resize', updateWindowSize);
-updateWindowSize();
